9024e4c594bb2f9079359a8f5aa71af3549a4cc1
Adamfomin-skc <96421014+Adamfomin-skc@users.noreply.github.com>
2022-01-12 10:33:27 +0000

    Add files via upload

---
+++ week 5 coding/Lists.py
@@ -0,0 +1,6 @@
+ALT = ['Adam F','Adam N','Oisin','Frank']
+print(ALT)
+List= ['David','Greg','Harry']
+newlist = ALT + List
+
+print(newlist)
---
+++ week 5 coding/random-rock, paper, scissors 181021.py
@@ -0,0 +1,4 @@
+import random
+answer= input('do you want to play rock paper scissors? ')
+if answer.strip().lower()== 'yes':
+    answer = input('Rock, Paper, or Scissors? ....')
